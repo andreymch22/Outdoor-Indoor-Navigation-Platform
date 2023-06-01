4369e16c3adc0e42bbee30b7d6b495fbaddddafe
Andrey Marín Chacón <andreymch22@gmail.com>
2023-06-01 09:37:39 -0600

    agregado react-navegation y logo de la app

--- App/App.js
+++ App/App.js
@@ -1,16 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import Main from './src/components/MapScreen';
-import Login from './src/components/Login'
+import Main from './src/components/Main';
+import { NavigationContainer } from '@react-navigation/native';
+import Login from './src/components/Login';
 import MapScreen from './src/components/MapScreen';
 
 
 export default function App() {
-  var flag = 1 // false= 0
-  if (flag) {
-    return <Login />
-  } else {
-    return <MapScreen />
-  }
+  return (
+    <NavigationContainer>
+      <Login />
+    </NavigationContainer>
+  );
 }
 
